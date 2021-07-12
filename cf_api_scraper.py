58d96cd211716e8aa800410c96b3c8d983e15a81
Bennett Novak <bobross419@gmail.com>
2021-07-11 21:01:42 -0400

    batch athletes to db

--- cf_api_scraper.py
+++ cf_api_scraper.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from pysondb import db
+from threading import thread
 
 athlete_db = 'db/athlete.json'
 affiliate_db = 'db/affiliate.json'
@@ -9,6 +10,9 @@ affiliate_url = 'https://games.crossfit.com/affiliate/'
 
 def add_athlete_to_db( athlete ):
     db.getDb(athlete_db).add(athlete)
+
+def add_athletes_to_db( atheletes ):
+    db.getDB(affiliate_db).addMany(athletes)
 
 def add_affiliate_to_db( affiliate ):
     db.getDb(affiliate_db).add(affiliate)
@@ -36,6 +40,39 @@ def get_total_pages( url ):
     pdata = fetch_page( url ).json()
     return pdata['pagination']['totalPages']
 
+def get_competition_threaded( year, division, scaled ):
+    api_years = { 
+            '2018': 'v1',
+            '2019': 'v1',
+            '2020': 'v1',
+            '2021': 'v2' }
+    api_roots = {
+            'v1': 'https://games.crossfit.com/competitions/api/v1',
+            'v2': 'https://c3po.crossfit.com/api/competitions/v2'}
+    api_comp_string =  '/competitions/open/'
+    api_search_string = '/leaderboards?scaled=' + str(scaled) + '&division=' + str(division) + '&page='
+
+    api_root = api_roots[api_years[year]]
+    api_url = api_root + api_comp_string + year + api_search_string
+
+    athletes = []
+
+    total_pages = get_total_pages( api_url + '1' )
+    threads = 4
+
+    for i in range(1, threads + 1):
+        start = ( total_pages * (i - 1) ) + 1
+        end = total_pages
+
+
+    for page in range(1, get_total_pages( api_url + '1') + 1):
+        pdata = fetch_page( api_url + str(page) ).json()
+        for row in pdata['leaderboardRows']:
+            athlete = row['entrant']
+            athlete.update({ 'overallRank': row['overallRank']})
+            athletes.append(athlete)
+            #add_athlete_to_db(athlete)
+        add_athletes_to_db(athletes)
 def get_competition( year, division, scaled ):
 
     api_years = { 
@@ -52,12 +89,16 @@ def get_competition( year, division, scaled ):
     api_root = api_roots[api_years[year]]
     api_url = api_root + api_comp_string + year + api_search_string
 
+    athletes = []
+
     for page in range(1, get_total_pages( api_url + '1') + 1):
         pdata = fetch_page( api_url + str(page) ).json()
         for row in pdata['leaderboardRows']:
             athlete = row['entrant']
             athlete.update({ 'overallRank': row['overallRank']})
-            add_athlete_to_db(athlete)
+            athletes.append(athlete)
+            #add_athlete_to_db(athlete)
+        add_athletes_to_db(athletes)
 
 def in_affiliate_db(affiliate_id, affiliates):
     return any(d['id'] == affiliate_id for d in affiliates)
